d724d7194d56054e19f20a6410b33fb0733b4c8d
bogusz2233 <bogusz2233@gmail.com>
2023-12-12 22:11:49 +0100

    Driver: add 'Write' and 'WriteImpl' methods

--- src/drivers/drivers.cpp
+++ src/drivers/drivers.cpp
@@ -1,22 +1,32 @@
 #include <drivers.h>
 
 namespace drivers {
-  
-  Result Driver::Read(void *data, uint32_t size){
-    if(data == nullptr){
-      return Status::NULLPTR;
-    }
 
-    if(size == 0){
-      return Status::INVALID_ARGUMENT;
-    }
-
-    return ReadImpl(data, size);
+Result Driver::Read(void *data, uint32_t size) {
+  if (data == nullptr) {
+    return Status::NULLPTR;
   }
 
+  if (size == 0) {
+    return Status::INVALID_ARGUMENT;
+  }
 
-Result Driver::ReadImpl(void *data, uint32_t size){
-  return Status::NOT_IMPLEMENTED;
+  return ReadImpl(data, size);
 }
 
+Result Driver::Write(const void *data, uint32_t size) {
+  if (data == nullptr) {
+    return Status::NULLPTR;
+  }
+
+  if (size == 0) {
+    return Status::INVALID_ARGUMENT;
+  }
+
+  return WriteImpl(data, size);
 }
+
+Result Driver::ReadImpl(void *data, uint32_t size) { return Status::NOT_IMPLEMENTED; }
+Result Driver::WriteImpl(const void *data, uint32_t size) { return Status::NOT_IMPLEMENTED; }
+
+} // namespace drivers
--- src/drivers/drivers.h
+++ src/drivers/drivers.h
@@ -6,11 +6,11 @@
 
 namespace drivers {
 
-enum class Status{
-  SUCCESS = 0,
-  FAIL = -1,
-  NOT_IMPLEMENTED = -2,
-  NULLPTR = -3,
+enum class Status {
+  SUCCESS          = 0,
+  FAIL             = -1,
+  NOT_IMPLEMENTED  = -2,
+  NULLPTR          = -3,
   INVALID_ARGUMENT = -4,
 };
 
@@ -19,10 +19,11 @@ using Result = std::variant<Status, uint32_t>;
 class Driver {
 public:
   Result Read(void *data, uint32_t size);
+  Result Write(const void *data, uint32_t size);
 
 private:
   virtual Result ReadImpl(void *data, uint32_t size);
-
+  virtual Result WriteImpl(const void *data, uint32_t size);
 };
 
 } // namespace drivers
